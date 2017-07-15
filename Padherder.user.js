2466a27a08b4084fc81f5830f009a3cc56e014b7
Michael David Pascual <mdnpascual@yahoo.com.ph>
2017-07-15 09:38:07 -0600

    added checks

--- Padherder.user.js
+++ Padherder.user.js
@@ -7,8 +7,8 @@
 // @match        https://www.padherder.com/*
 // @require      http://userscripts-mirror.org/scripts/source/107941.user.js
 // @require      http://code.jquery.com/jquery-3.2.1.js
-// @downloadURL  https://github.com/mdnpascual/Padherder_Extender/raw/master/Padherder.user.js
-// @updateURL    https://github.com/mdnpascual/Padherder_Extender/raw/master/Padherder.user.js
+// @downloadURL  https://github.com/mdnpascual/padherderExtender/raw/master/Padherder_test.user.js
+// @updateURL    https://github.com/mdnpascual/padherderExtender/raw/master/Padherder_test.user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @run-at       document-end
@@ -222,9 +222,11 @@
                     var t = 1;
                     var status = true;
                     while (t < splitting.length){
-                        if (mats_format.length != 1 && mats_format[t][0] == splitting[1]){
-                            status = false;
-                            break;
+                        if ("undefined" !== typeof mats_format[t]){
+                            if (mats_format[t][0] == splitting[1]){
+                                status = false;
+                                break;
+                            }
                         }
                         t++;
                     }
