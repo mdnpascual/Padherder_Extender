9603dcbf726931ff6afe22576c2ae642481dd874
Michael David Pascual <mdnpascual@yahoo.com.ph>
2017-08-10 08:49:08 -0600

    keep messing up update url <,<

--- Padherder.user.js
+++ Padherder.user.js
@@ -7,8 +7,8 @@
 // @match        https://www.padherder.com/*
 // @require      http://userscripts-mirror.org/scripts/source/107941.user.js
 // @require      http://code.jquery.com/jquery-3.2.1.js
-// @downloadURL  https://github.com/mdnpascual/padherderExtender/raw/master/Padherder_test.user.js
-// @updateURL    https://github.com/mdnpascual/padherderExtender/raw/master/Padherder_test.user.js
+// @downloadURL  https://github.com/mdnpascual/Padherder_Extender/raw/master/Padherder.user.js
+// @updateURL    https://github.com/mdnpascual/Padherder_Extender/raw/master/Padherder.user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @run-at       document-end
