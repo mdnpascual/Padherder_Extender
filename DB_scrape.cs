0d29f09a4c8913fd0a11a5435439cab67550ee74
mdnpascual <mdnpascual@yahoo.com.ph>
2018-04-02 01:27:02 -0600

    Database Update 4/2

--- DB_scrape.cs
+++ DB_scrape.cs
@@ -144,14 +144,18 @@ namespace Padguide_Scrape
             DateTime thisDay = DateTime.Today.AddHours(1);
             test.Clear();
             test.Append(((int)thisDay.DayOfWeek).ToString() + "\r\n");
-            test.Append("February Quest - Hero Descended!::: 191,193,195,197,199,171,172,173,174,175,69,71,73,75,77,110,221,223,\r\nFebruary Quest - The Goddess Descended!::: 191,193,195,197,199,171,172,173,174,175,234,86,475,476,477,225,\r\nFebruary Quest - Dark Knight!::: 792,906,101,109,1647,\r\nFebruary Quest - Great Tengu Descended!::: 550,689,311,717,718,681,545,547,549,551,553,580,777,\r\nFebruary Quest - Guan Yinping!::: 545,547,549,551,553,1527,1530,717,718,681,1526,1528,1876,1589,\r\nFebruary Quest - Draggie!::: 36,38,40,42,44,687,688,37,39,41,43,45,684,685,686,742,\r\nFebruary Quest - Dragon Zombie::: 578,603,1063,1061,\r\nFebruary Quest - Zeus Vulcan Descended!::: 788,790,794,23,399,222,224,598,1251,\r\nFebruary Quest - Heracles Descended!::: 406,107,94,1097,\r\nFebruary Quest - Izanami Descended!::: 2216,827,829,811,813,\r\nFebruary Quest - Athena Descended!::: 107,224,222,648,\r\nFebruary Quest - Takeminakata Descended!::: 545,547,549,551,553,827,829,809,\r\nFebruary Quest - Hera-Is Descended!::: 476,479,217,220,412,284,33,661,596,\r\nFebruary Quest - Kanetsugu!::: 717,718,779,545,547,549,551,553,550,552,554,1528,1530,689,681,541,542,1524,\r\nFebruary Quest - Azazel Descended!::: 2313,2583,3209,1749,300,3456,\r\nFebruary Quest - Serket Descended!::: 2007,2823,1917,1961,3258,3456,\r\nFebruary Quest - Khepri Descended!::: 788,1171,1172,1173,1174,1175,1542,1466,2723,2724,2725,2726,2727,1169,1186,586,1836,2007,3158,\r\nFebruary Quest - Liberty Geist Descended!::: 80,2195,2197,2581,2647,2649,320,3014,2886,3016,\r\nFebruary Quest - Hanuman Descended!::: 2837,\r\nFebruary Quest - Seraphis Descended!::: 2652,2654,2656,2585,2643,2650,783,2890,\r\nFebruary Quest - Shuten-doji Descended!::: 161,166,170,234,2305,1529,1468,1467,1765,2804,\r\nFebruary Quest - Jormungandr Descended!::: 1171,1172,1173,1174,1175,2313,475,476,477,478,479,1170,1064,790,88,2638,\r\nFebruary Quest - Gran Reverse Descended!::: 32,35,971,1132,1133,1134,1135,2736,\r\nFebruary Quest - Yamatsumi Descended!::: 545,547,549,551,553,2307,2309,1527,1528,774,541,2548,\r\nFebruary Quest - Journey to the West::: 545,547,549,551,553,2183,2181,1247,1245,\r\nFebruary Quest - Sphinx Descended!::: 788,794,403,1462,1464,2007,\r\nFebruary Quest - Sonia Gran Descended!::: 23,26,29,772,773,774,775,776,1759,\r\nFebruary Quest - Zeus Mercury Descended!::: 317,907,1545,788,401,263,265,267,269,271,596,1531,\r\n");
+            //test.Append("February Quest - Hero Descended!::: 191,193,195,197,199,171,172,173,174,175,69,71,73,75,77,110,221,223,\r\nFebruary Quest - The Goddess Descended!::: 191,193,195,197,199,171,172,173,174,175,234,86,475,476,477,225,\r\nFebruary Quest - Dark Knight!::: 792,906,101,109,1647,\r\nFebruary Quest - Great Tengu Descended!::: 550,689,311,717,718,681,545,547,549,551,553,580,777,\r\nFebruary Quest - Guan Yinping!::: 545,547,549,551,553,1527,1530,717,718,681,1526,1528,1876,1589,\r\nFebruary Quest - Draggie!::: 36,38,40,42,44,687,688,37,39,41,43,45,684,685,686,742,\r\nFebruary Quest - Dragon Zombie::: 578,603,1063,1061,\r\nFebruary Quest - Zeus Vulcan Descended!::: 788,790,794,23,399,222,224,598,1251,\r\nFebruary Quest - Heracles Descended!::: 406,107,94,1097,\r\nFebruary Quest - Izanami Descended!::: 2216,827,829,811,813,\r\nFebruary Quest - Athena Descended!::: 107,224,222,648,\r\nFebruary Quest - Takeminakata Descended!::: 545,547,549,551,553,827,829,809,\r\nFebruary Quest - Hera-Is Descended!::: 476,479,217,220,412,284,33,661,596,\r\nFebruary Quest - Kanetsugu!::: 717,718,779,545,547,549,551,553,550,552,554,1528,1530,689,681,541,542,1524,\r\nFebruary Quest - Azazel Descended!::: 2313,2583,3209,1749,300,3456,\r\nFebruary Quest - Serket Descended!::: 2007,2823,1917,1961,3258,3456,\r\nFebruary Quest - Khepri Descended!::: 788,1171,1172,1173,1174,1175,1542,1466,2723,2724,2725,2726,2727,1169,1186,586,1836,2007,3158,\r\nFebruary Quest - Liberty Geist Descended!::: 80,2195,2197,2581,2647,2649,320,3014,2886,3016,\r\nFebruary Quest - Hanuman Descended!::: 2837,\r\nFebruary Quest - Seraphis Descended!::: 2652,2654,2656,2585,2643,2650,783,2890,\r\nFebruary Quest - Shuten-doji Descended!::: 161,166,170,234,2305,1529,1468,1467,1765,2804,\r\nFebruary Quest - Jormungandr Descended!::: 1171,1172,1173,1174,1175,2313,475,476,477,478,479,1170,1064,790,88,2638,\r\nFebruary Quest - Gran Reverse Descended!::: 32,35,971,1132,1133,1134,1135,2736,\r\nFebruary Quest - Yamatsumi Descended!::: 545,547,549,551,553,2307,2309,1527,1528,774,541,2548,\r\nFebruary Quest - Journey to the West::: 545,547,549,551,553,2183,2181,1247,1245,\r\nFebruary Quest - Sphinx Descended!::: 788,794,403,1462,1464,2007,\r\nFebruary Quest - Sonia Gran Descended!::: 23,26,29,772,773,774,775,776,1759,\r\nFebruary Quest - Zeus Mercury Descended!::: 317,907,1545,788,401,263,265,267,269,271,596,1531,\r\n");
+            //test.Append("March Quest - Rushana Descended!-No Dupes::: 3212,3213,3214,3215,3216,2181,2430,2431,3835,3836,3837,3207,3208,3209,3210,3211,3966,\r\nMarch Quest - Kuramitsuha Descended!-No Skyfall combos::: 2309,1530,2545,2546,544,3152,3250,\r\nMarch Quest - Gran Reverse Descended!-Dragon Enhanced::: 32,35,971,1132,1133,1134,1135,2736,\r\nMarch Quest - Surtr Descended!-All Att. Req::: 2311,2313,1224,2397,\r\nMarch Quest - Zeus & Hera Descended!::: 684,685,686,1922,\r\nMarch Quest - Medjedra Descended!::: 161,172,173,794,788,234,1466,1836,\r\nMarch Quest - Wadatsumi Descended!-Awoken Skills Invalid::: 548,690,310,717,790,90,1306,\r\nMarch Quest - Hera-Beorc Descended!::: 792,92,1188,\r\nMarch Quest - Sandalphon Descended!-No Dupes::: 404,94,1118,\r\nMarch Quest - Hera Descended!::: 191,192,193,194,195,196,197,198,199,200,189,\r\n");
+            test.Append("April Quest - Ras Descended!::: 521,579,1964,250,98,102,2881,318,782,3328,\r\nApril Quest - Mion Descended!::: 2196,2197,3063,294,974,1118,3220,\r\nApril Quest - Cosmo Crusader Descended! - No RCV::: 2195,2197,2653,86,2336,105,319,3154,2889,3156,\r\nApril Quest - Hanuman Descended! - No Dupes::: 2837,\r\nApril Quest - Hephaestus Descended!::: 794,906,907,908,909,910,99,1541,1542,2721,\r\nApril Quest - Hel Descended!::: 796,2305,2311,32,1061,2401,\r\nApril Quest - Deus ex Machina Descended!!::: 2198,2195,283,284,285,286,287,782,783,784,785,786,285,287,316,317,318,319,2233,2888,\r\nApril Quest - Jord Descended!::: 153,227,690,691,788,101,792,2319,\r\nApril Quest - Aamir Descended!::: 191,193,195,197,199,1527,1528,475,476,477,478,479,1771,1773,1775,1777,1779,206,207,208,209,210,1749,2068,\r\nApril Quest - Zhou Yu!! - All Att Needed::: 1370,681,1527,1529,399,2103,\r\nApril Quest - Sonia Gran Descended!::: 23,26,29,772,773,774,775,776,1759,\r\nApril Quest - Thoth & Sopdet Descended!::: 147,148,149,150,151,321,403,896,898,900,902,904,1462,1464,\r\nApril Quest - Noah Descended! - All Att Needed::: 304,306,308,601,603,1178,1180,1182,1184,1186,263,265,267,269,271,398,400,402,404,406,26,1460,\r\nApril Quest - Hera-Sowilo Descended!::: 191,193,195,197,199,793,98,100,102,104,108,794,910,199,793,98,100,102,104,108,200,32,200,110,478,479,648,1249,\r\nApril Quest - Heracles Descended!::: 406,107,94,1097,\r\nApril Quest - Beelzebub Descended!::: 406,644,\r\nApril Quest - Zeus-Dios Descended!::: 221,223,402,94,189,650,\r\nApril Quest - The Thief Descended!::: 545,547,549,545,547,549,546,548,550,546,548,550,565,\r\nApril Quest - General Yan - Physical & Attacker Enhanced::: 689,690,691,546,548,550,540,542,1716,1424,\r\nApril Quest - Dragon Zombie - Dragon Enhanced::: 578,603,1063,1061,\r\n");
+            List<String> ignoreEvents = new List<string>(){ "1", "25", "65" };
             Console.WriteLine(thisDay.ToString("yyyy-MM-dd"));
             List<List<String>> Dungeon = CSV_Parser(path + "\\TBL_DUNGEON.csv");
             List<List<String>> DungeonMonster = CSV_Parser(path + "\\TBL_DUNGEON_MONSTER.csv");
             List<List<String>> Schedule = CSV_Parser(path + "\\TBL_SCHEDULE.csv");
             List<List<String>> SubDungeon = CSV_Parser(path + "\\TBL_SUB_DUNGEON.csv");
+            List<List<String>> ExtraDrop = CSV_Parser(path + "\\TBL_DUNGEON_MONSTER_DROP.csv");
             //Filtering stuff
-            List<List<String>> USonly = Schedule.Where(o => (o[3] == "US") && (o[2] == "0")).ToList();
+            List<List<String>> USonly = Schedule.Where(o => (o[3] == "US") && !(ignoreEvents.Contains(o[2]))).ToList();
             List<List<String>> ValidCloseTime = USonly.Where(o => DateTime.Parse(o[14], CultureInfo.CreateSpecificCulture("en-US")).AddHours(-7) >= thisDay.AddHours(3)).ToList();
             List<List<String>> ValidOpenTime = ValidCloseTime.Where(o => DateTime.Parse(o[9], CultureInfo.CreateSpecificCulture("en-US")).AddHours(-7) < thisDay.AddDays(1)).ToList();
 
@@ -165,17 +169,24 @@ namespace Padguide_Scrape
                 while (j < Difficulty.Count)
                 {
                     //test += DungeonName + " Lvl." + Difficulty[j][3] + "::: ";
-                    if (test.ToString().Contains(Decrypt(Difficulty[j][5], "<Replace with AESKEY>") + "::: "))
+                    if (test.ToString().Contains(Decrypt(Difficulty[j][5], "<16LENGTHAESKEY>") + "::: "))
                     {
                         j++;
                         continue;
                     }
-                    test.Append(Decrypt(Difficulty[j][5], "<Replace with AESKEY>") + "::: ");
+                    test.Append(Decrypt(Difficulty[j][5], "<16LENGTHAESKEY>") + "::: ");
                     List<List<String>> Drop = DungeonMonster.Where(o => (o[3] == Difficulty[j][0]) && (o[2] == DungeonID)).ToList();
                     int k = 0;
                     List<String> DropMons = new List<String>();
                     while (k < Drop.Count)
                     {
+                        List<List<String>> Drop2 = ExtraDrop.Where(o => o[5] == Drop[k][0]).ToList();
+                        int l = 0;
+                        while (l < Drop2.Count)
+                        {
+                            DropMons.Add(Drop2[l][3]);
+                            l++;
+                        }
                         DropMons.Add(Drop[k][11]);
                         k++;
                     }
@@ -216,7 +227,7 @@ namespace Padguide_Scrape
             req.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
             // req.Headers.Add(string.Format("Authorization: token {0}", oauthToken));
             //req.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(new ASCIIEncoding().GetBytes(username + ":" + password)));
-            req.Headers.Add("Authorization: token <gist token auth>");
+            req.Headers.Add("Authorization: token 29862dbc2dfd4fe19d23f797f1254ef3aa71c9d7");
             req.UserAgent = "Anything";
             StreamWriter writer = new StreamWriter(req.GetRequestStream());
 
@@ -224,9 +235,9 @@ namespace Padguide_Scrape
             writer.Write(jsonMessage.ToString());
             writer.Close();
 
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
             string result = null;
-			//Github change to only use TLS1.2 and above: https://githubengineering.com/crypto-removal-notice/
-			ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
             using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)//Exception here
             {
                 StreamReader reader =
@@ -235,7 +246,6 @@ namespace Padguide_Scrape
                 System.Diagnostics.Debug.WriteLine(result);
             }
             
-            i++;
         }
 
         private static string getMats(List<List<String>> EvoMat, string tv_seq)
